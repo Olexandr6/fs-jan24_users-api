7659b93320668ae5e443ff3faef5671f3a1e7cc2
Sasha Yatsykovych <olexandr.y@mate.academy>
2024-05-20 13:59:22 +0300

    initial code

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import { Color, User } from './types';
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5005;
 const app = express();
 
 app.use(cors({ origin: '*' }));
